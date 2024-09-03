a978ef9274e4ef235ca76266e5204539588fffa2
leanmontero <leangmontero@gmail.com>
2024-09-03 05:11:14 -0300

    create uppercase directive

---
+++ src/app/directives/uppercase.directive.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appUppercase]',
+})
+
+export class UppercaseDirective {
+  @HostListener('input', ['$event']) onInput(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    input.value = input.value.toUpperCase();
+  }
+}
+
+@NgModule({
+  declarations: [UppercaseDirective],
+  exports: [UppercaseDirective],
+  imports: [CommonModule],
+})
+export class DirectiveModule {}
